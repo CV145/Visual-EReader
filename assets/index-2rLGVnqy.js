c01766f928245fccd9cfa5e7b561c0e6045f69e1
Carlos Valeriano <cvaleriano564@gmail.com>
2026-04-11 19:17:42 -0500

    Updates

--- assets/index-2rLGVnqy.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"./index-C4wx5WM_.js";import{B as e,C as r,E as n,L as p,R as u}from"./index-C4wx5WM_.js";export{e as Book,r as Contents,n as EpubCFI,p as Layout,u as Rendition,a as default};
---
+++ assets/index-xcRuzHBL.js
@@ -0,0 +1,1 @@
+import{e as a}from"./index-DObGKshE.js";import{B as e,C as r,E as n,L as p,R as u}from"./index-DObGKshE.js";export{e as Book,r as Contents,n as EpubCFI,p as Layout,u as Rendition,a as default};
